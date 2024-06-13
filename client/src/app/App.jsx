8a79911a11c9b5aecb6a4779896b71d115d6fcda
Dmitriy Popov <145620090+iLooToS@users.noreply.github.com>
2024-06-14 00:43:30 +0300

    fix/theme-pharams

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,23 +1,23 @@
-import Main from '../page/main/Main';
-import Navbar from '../page/navbar/Navbar';
-import Questions from '../page/questions/Questions';
-import './App.css'
-import { Route, Routes } from 'react-router-dom';
+import Main from "../page/main/Main";
+import Navbar from "../page/navbar/Navbar";
+import Questions from "../page/questions/Questions";
+import Themes from "../page/themes/Themes";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route
-          path='/questions/:id'
-          element={<Questions />}
-        />
-        <Route path='*' element={<h1>404</h1>}/>
+        <Route path="/" element={<Main />} />
+        <Route path="/questions/:id" element={<Questions />} />
+        <Route path="/themes/:id/questions" element={<Questions />} />
+        <Route path="/themes" element={<Themes />} />
+        <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
