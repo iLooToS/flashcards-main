9e8dd782283fa8bd0624df85759da9ef21ecfbf7
MashaShamash <mariapanasenko26@yandex.ru>
2024-06-14 11:26:39 +0300

    done

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const removeHeaders = require('../middleware/removeHeaders');
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const removeHeaders = require("../middleware/removeHeaders");
+const path = require("path");
+const morgan = require("morgan");
 const app = express();
 
-const indexRouter = require('../routes/index.routes');
+const indexRouter = require("../routes/index.routes");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(removeHeaders);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use('/api', indexRouter);
+app.use("/api", indexRouter);
 
 module.exports = app;
